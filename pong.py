22cf73d8094787c0b55fc0964bb6460ec5927749
Arthur Evensen <evensen.arthur.n@gmail.com>
2018-02-10 14:00:28 +0100

    Add score display

--- pong.py
+++ pong.py
@@ -1,5 +1,6 @@
 import pygame
 import colour_constants
+import random
 
 SCREEN_SIZE = (640, 480)
 BALL_DIAMETER = 32
@@ -16,11 +17,8 @@ PUREBLACK = colour_constants.PUREBLACK #Easy access for colorkeying
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 
-def draw_play_background() -> None:
-    screen.fill(BLACK) 
-
 class Play_Background:
-    def __init__(self) ->None:
+    def __init__(self):
         self.surface = pygame.Surface(SCREEN_SIZE)
         self.surface.fill(BLACK)
         self.surface = self.surface.convert()
@@ -37,7 +35,7 @@ class vector2:
         self.y = y
 
 class Paddle:
-    def __init__(self, side, player, up_button=None, down_button=None) -> None:
+    def __init__(self, side, player, up_button=None, down_button=None):
         if side == "left":
             self.pos = vector2(SCREEN_SIZE[0]//40, SCREEN_SIZE[1]//2 - PADDLE_SIZE[1]//2)
         elif side == "right":
@@ -54,6 +52,7 @@ class Paddle:
         self.surface = pygame.Surface( (PADDLE_SIZE[0], PADDLE_SIZE[1]) )
         self.surface.fill(COLOUR)
         self.surface = self.surface.convert()
+        self.score = 0
 
     def move(self, time_passed) -> None:
         #Accelerate
@@ -84,28 +83,36 @@ class Paddle:
 
 
 class Ball:
-    def __init__(self) -> None:
+    def reset(self) -> None:
+        self.recent_hit = 0
         self.pos = vector2(SCREEN_SIZE[0]//2, SCREEN_SIZE[1]//2)
-        self.vel = vector2(SCREEN_SIZE[0]/2,  SCREEN_SIZE[1]/2)
+        self.vel = vector2(SCREEN_SIZE[0] * random.random(), SCREEN_SIZE[1] * random.random())
+
+    def __init__(self):
+        self.pos = None
+        self.vel = None
         self.surface = pygame.Surface( (BALL_DIAMETER, BALL_DIAMETER) )
         self.surface.set_colorkey(PUREBLACK)
         pygame.draw.circle( self.surface, COLOUR, (BALL_DIAMETER//2, BALL_DIAMETER//2), BALL_DIAMETER//2)
         self.surface = self.surface.convert_alpha()
-        self.recent_hit = 0
+        self.recent_hit = None
+        self.reset()
 
     def horizontal_bar_check_and_adjustment(self) -> None:
-        if self.pos.x < 0 or self.pos.x + BALL_DIAMETER > SCREEN_SIZE[0]:
-            self.vel.x *= -1
-            if self.pos.x < 0:
-                self.pos.x = 0
-            else:
-                self.pos.x = SCREEN_SIZE[0] - BALL_DIAMETER
         if self.pos.y < HORIZONTAL_BAR[1] or self.pos.y + BALL_DIAMETER > SCREEN_SIZE[1] - HORIZONTAL_BAR[1]:
             self.vel.y *= -1
             if self.pos.y < HORIZONTAL_BAR[1]:
                 self.pos.y = HORIZONTAL_BAR[1]
             else:
                 self.pos.y = SCREEN_SIZE[1] - BALL_DIAMETER - HORIZONTAL_BAR[1]
+
+    def score_check(self) -> str: #Can also return None!
+        if self.pos.x + BALL_DIAMETER < 0:
+            return "right"
+        elif self.pos.x > SCREEN_SIZE[0]:
+            return "left"
+        else:
+            return None
 
     def simple_collision_check(self, paddles) -> Paddle: #Can also return None!
         left = paddles[0]
@@ -134,7 +141,7 @@ class Ball:
 
     def collision_handling(self, paddle) -> None:
         print("The ball collided with the", paddle.side, "paddle!")
-        self.vel.x *= -1.1
+        self.vel.x *= -1.05
         #TODO: Implement fun system calculating new velocities depending on where on the paddle the ball hit.
 
     def move(self, time_passed, paddles) -> None:
@@ -154,14 +161,25 @@ class Ball:
 
         
 
+class Scoredrawer:
+    def __init__(self):
+        self.left = 0
+        self.right = 0
+        self.font = pygame.font.SysFont("Mono", 32)
 
-
+    def draw(self, left, right): #Idea for improved performance: Check whether there's a change to a score and then only rendering if so.
+        left_text  = self.font.render(str(left),  True, COLOUR).convert_alpha()
+        right_text = self.font.render(str(right), True, COLOUR).convert_alpha()
+        y_offset = HORIZONTAL_BAR[1] * 1.5
+        screen.blit(left_text,  (SCREEN_SIZE[0]//2 - 32 - left_text.get_width(),  y_offset))
+        screen.blit(right_text, (SCREEN_SIZE[0]//2 + 32, y_offset))
 
 
 def play() -> None:
     clock = pygame.time.Clock()
 
     background = Play_Background()
+    scoredrawer = Scoredrawer()
     ball = Ball()
     paddle1 = Paddle(side="left", player=True, up_button=pygame.K_q, down_button=pygame.K_a)
     paddle2 = Paddle(side="right", player=True, up_button=pygame.K_o, down_button=pygame.K_l)
@@ -181,8 +199,17 @@ def play() -> None:
 
         ball.move(time_passed, paddles)
         for paddle in paddles: paddle.move(time_passed)
+        
+        scorer = ball.score_check() #Either "left", "right" or None
+        if scorer:
+            if scorer == "left":
+                paddles[0].score += 1
+            else:
+                paddles[1].score += 1
+            print(scorer, "has scored! Current score: Left -", paddles[0].score, " :: Right -", paddles[1].score)
+            ball.reset()
 
-
+        scoredrawer.draw(paddles[0].score, paddles[1].score)
         ball.draw()
         for paddle in paddles: paddle.draw()
         pygame.display.update()
